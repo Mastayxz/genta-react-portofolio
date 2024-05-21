188f104e5a2c1a4f34c99c8555fadf4c80ca029b
Mastayxz <gustibagus826@gmail.com>
2024-05-21 15:33:31 +0800

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <h3 className="text-2xl mx-2 font-bold">Genta Arimbawa</h3>
+        <h3 className="text-2xl mx-auto font-bold">Genta Arimbawa</h3>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/genta-arimbawa">
